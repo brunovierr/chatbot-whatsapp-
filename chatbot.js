d2221742cb930e5c183e7165dc498cc7f1933833
Bruno de Franca Vieira <bruno.fv.08@gmail.com>
2026-05-05 18:44:06 -0300

    add alerta de solicitacao de atendimento + att no fluxo/funil de msg

--- chatbot.js
+++ chatbot.js
@@ -42,21 +42,21 @@ client.initialize();
 // =====================================
 // FUNÇÃO DE DELAY
 // =====================================
-const delay = (ms) => new Promise((res) => setTimeout(res, ms)); // preciso entender esta linha
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+const userEstado = {}; // para que 
 
-// =====================================
-// FUNIL DE MENSAGENS (SOMENTE PRIVADO)
-// =====================================
+// MENSAGENS
 client.on("message", async (msg) => {
+
   try {
     if (!msg.from || msg.from.endsWith("@g.us")) return;
-
     const chat = await msg.getChat();
     if (chat.isGroup) return;
 
     const texto = msg.body ? msg.body.trim().toLowerCase() : "";
-    
-    // Função de digitação
+    const userId = msg.from;
+
+    // SIMULA DIGITACAO
     const typing = async () => {
       await delay(2000);
       await chat.sendStateTyping();
@@ -67,18 +67,17 @@ client.on("message", async (msg) => {
     // MENSAGEM INICIAL
     // =====================================
     if (/^(teste)$/i.test(texto)) {
-
-      
+      // if (/^(teste|ol[aá]|dia|tarde|noite|oi|bom\s+dia|boa\s+tarde|boa\s+noite|opa|salve|hey|hi)$/i.test(texto)) {
       const hora = new Date().getHours();
       let saudacao = "Olá";
-      
+
       if (hora >= 5 && hora < 12) saudacao = "Bom dia";
       else if (hora >= 12 && hora < 18) saudacao = "Boa tarde";
       else saudacao = "Boa noite";
-      
+
       const contact = await msg.getContact();
       const nome = contact.pushname;
-      
+
       await delay(1000);
       await chat.sendStateTyping();
       await delay(500);
@@ -87,10 +86,10 @@ client.on("message", async (msg) => {
         msg.from,
         `${saudacao} ${nome}! 👋`
       );
-      
-      await typing(); // usa a funcao de digitação criada
 
-      await client.sendMessage(
+      await typing();
+      userEstado[userId] = "MENU";
+      return await client.sendMessage(
         msg.from,
         `Seja bem-vindo(a) à Med Clean. 🧺
 
@@ -103,42 +102,86 @@ Como podemos ajudar você hoje? Digite o número da opção desejada:
 5. Outras dúvidas / Falar com um Atendente.`
       );
     }
-    
-    
-    if (msg.body === '1'){
-      client.sendMessage(msg.from, 
-        `Você pode conferir todos os nossos serviços e valores atualizados no nosso catálogo digital clicando aqui: https://drive.google.com/file/d/1_sk0v19Q7mi3eZ3zOcHvLH7lG7Y6tElB/view?usp=sharing`
-      );
+    if (userEstado[userId] === "MENU") {
+      if (msg.body === '1') {
+        // if (/(pre[cç]o|valor|tabela|quanto|custa|servi[çc]o)/i.test(texto) | msg.body === '1') {
+
+        const media = MessageMedia.fromFilePath('./media/tabela.png');
+
+        await delay(500);
+        await chat.sendStateTyping();
+        await delay(500);
+
+        client.sendMessage(msg.from, media);
+
+        await delay(1000);
+        await chat.sendStateTyping();
+        await delay(500);
+
+        client.sendMessage(msg.from,
+          `Aqui está nossa tabela de preços atualizada! 🧺`
+        );
+      };
+
+      if (msg.body === '2') {
+
+        await delay(1000);
+        await chat.sendStateTyping();
+        await delay(500);
+
+        client.sendMessage(msg.from,
+          `📞 O nosso telefone de contato é *61 3301-1443*.`
+        );
+      };
+
+      if (msg.body == '3') {
+        const localizacao = new Location(-15.842678653156582, -47.98440178074539, 'Lavanderia Med Clean');
+
+        await delay(1000);
+        await chat.sendStateTyping();
+        await delay(500);
+
+        client.sendMessage(msg.from, localizacao);
+        client.sendMessage(msg.from,
+          `📍 Estamos localizados no Guará II, no endereço:\nQE 40 Conjunto D Lote 20 - Guará, Brasília - DF.`);
+      };
+
+      if (msg.body == '4') {
+
+        await delay(1000);
+        await chat.sendStateTyping();
+        await delay(500);
+
+        client.sendMessage(msg.from,
+          `Nosso horário de funcionamento é de segunda a sexta, das *08:00 às 18:00*, e aos sábados, das *08:00 às 12:00*.`
+        );
+      };
+
+      if (msg.body == '5') {
+        const contact = await msg.getContact();
+        const nome = contact.pushname;
+        const horario = new Date().toLocaleString('pt-BR');
+        const numeroCliente = contact.id.user;
+
+        const alerta = `*🚨Notificação de atendimento*\n *Cliente:* ${nome} solicitou atendimento.\n *Número:* ${numeroCliente}.\n *Hora:* ${horario}`;
+        client.sendMessage(client.info.wid._serialized, alerta);
+
+        await delay(1000);
+        await chat.sendStateTyping();
+        await delay(500);
+
+        const hora = new Date().toLocaleTimeString('pt-BR');
+        if (hora >= 8 && hora <= 18) {
+          client.sendMessage(msg.from,
+            `Entendido! Em instantes, um de nossos atendentes dará continuidade à conversa.\nPor favor, aguarde só um momento! ⏳`);
+        } else {
+          client.sendMessage(msg.from,
+            `Agradecemos o seu contato, porém no momento não há atendentes disponíveis.🫤\n\nNosso horário de funcionamento é de segunda a sexta *08:00 às 18:00* e aos sábados *08:00 às 12:00*.\nPor favor, deixe sua mensagem e retornaremos o contato o mais breve possível!`
+          );
+        };
+      };
     };
-    
-    if (msg.body === '2'){
-      client.sendMessage(msg.from,
-        `📞 Nosso telefone de contato é:\n*61 3301-1443*`
-  );
-};
-  
-if (msg.body == '3'){
-const localizacao = new Location(-15.842678653156582, -47.98440178074539, 'Lavanderia Med Clean');
-  client.sendMessage(msg.from, localizacao);
-  client.sendMessage(msg.from, 
-    `📍 Estamos localizados no Guará II, no endereço:\nQE 40 Conjunto D Lote 20 - Guará, Brasília - DF`);
-};
-
-if (msg.body == '4'){
-  client.sendMessage(msg.from,
-    `Nosso atendimento funciona de segunda a sexta, das *08:00 às 18:00*, e aos sábados, das *08:00 às 12:00*.`
-  );
-};
-
-if (msg.body == '5'){
-  client.sendMessage(msg.from,
-    `Entendido! Em instantes, um de nossos atendentes dará continuidade à conversa.\nPor favor, aguarde só um momento! ⏳`
-  );
-};
-
-
-
-} catch (error) {
-  console.error("Erro no processamento da mensagem:", error);
-}
-});
+    } catch (error) {
+      console.error("Erro no processamento da mensagem:", error);
+    }
+  });
